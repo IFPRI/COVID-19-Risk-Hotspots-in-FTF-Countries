d1c6a44ef2faee96a2b831cbf4233e9af89f16e1
anighosh <ani.biogeo@gmail.com>
2020-09-02 20:43:26 +0300

    m

--- processing/4_combine_risk_parameters.R
+++ processing/4_combine_risk_parameters.R
@@ -2,8 +2,12 @@
 library(raster)
 library(sf)
 
-saveAllRisk <- function(iso, dir){
-  cat("processing", iso, "\n")
+saveAllRisk <- function(i, ciso, dir){
+  country <- ciso[i,"country"]
+  iso <- ciso[i,"iso"]
+  
+  cat("processing ", country, "\n")
+  
   # output directory
   odir <- file.path(dir, "output/final")
   dir.create(odir, FALSE, TRUE)
@@ -21,24 +25,31 @@ saveAllRisk <- function(iso, dir){
   ffd <- Reduce(function(x, y) merge(x, y, all.x = TRUE, by=c("NAME_1","NAME_2")), fd, accumulate = FALSE)
   # get rid of any specific name
   names(ffd) <- gsub(paste0(iso,"_"), "", names(ffd))
-  # add iso
-  ffd <- data.frame(iso3=iso, ffd, stringsAsFactors = FALSE)
-  # save csv
-  write.csv(ffd, file = file.path(odir, paste0(iso, "_all_risk.csv")), row.names = FALSE)
   
   # merge with spatial
   vsf <- merge(vsp, ffd, by = c("NAME_1","NAME_2"), all.x=TRUE)
+  # add iso/country
+  vsf@data <- data.frame(country = country, iso3 = iso, ffd, stringsAsFactors = FALSE)
+  # save csv
+  write.csv(vsf@data, file = file.path(odir, paste0(iso, "_all_risk.csv")), row.names = FALSE)
   
   vsf <- st_as_sf(vsf)
-  st_write(vsf, file.path(odir, paste0(iso, "_all_risk.geojson")))
+  st_write(vsf, file.path(odir, paste0(iso, "_all_risk.geojson")), overwrite = TRUE, append = FALSE)
 }
 
+#############################################################################################
+# input
 dir <- "C:/Users/anibi/Documents/work/covid_hotspot/data"
+# country
+countries <- c("Bangladesh", "Ethiopia", "Ghana", "Guatemala", 
+               "Honduras", "Kenya", "Mali", "Nepal",
+               "Niger", "Nigeria", "Senegal", "Uganda")
 iso3 <- c("BGD", "ETH", "GHA", "GTM", "HND", "KEN", "MLI", "NPL", "NER", "NGA", "SEN", "UGA")
+ciso <- data.frame(country = countries, iso = iso3, stringsAsFactors = FALSE)
 
-lapply(iso3, saveAllRisk, dir)
+lapply(1:nrow(ciso), saveAllRisk, ciso, dir)
 
-
+#############################################################################################
 # merge geojson objects to create a single one
 jj <- list.files(file.path(dir, "output/final"), pattern = ".geojson", full.names = TRUE)
 # read all except NIGER
@@ -49,7 +60,7 @@ asj <- do.call(rbind, sj)
 st_write(asj, file.path(dir, "output/final/all_countries_risk_exNER.geojson"))
 
 # zip everything for share
-zz <- list.files(file.path(dir, "output/final"), pattern = ".geojso", full.names = TRUE)
+zz <- list.files(file.path(dir, "output/final"), pattern = ".geojson", full.names = TRUE)
 zname <- paste0("all_countries_geojson_", Sys.time(), ".zip")
 zname <- gsub(":| ","_",zname)
 zip(file.path(dir, "output/final", zname), zz, flags = " a -tzip",
